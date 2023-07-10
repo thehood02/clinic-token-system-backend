f0fb5bf6b96f27d0e74daacd1de8e6fd4ad24815
thehood02 <yash28847@gmail.com>
2023-07-10 14:08:53 +0530

    change database and collection name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URI;
+const mongoString = process.env.DATABASE_URI + process.env.DB_NAME;
 
 
 mongoose.connect(mongoString);
--- model/model.js
+++ model/model.js
@@ -1,23 +1,22 @@
 const mongoose = require("mongoose");
 
 const dataSchema = new mongoose.Schema({
-    name: {
-        required: true,
-        type: String
-    },
-    phoneNumber: {
-        required: true,
-        type: String
-    },
-    // waiting, ongoing, complete
-    status: {
-        required: false,
-        type: String
-    },
-    tokenId: {
-        required: false,
-        type: String
-    }
+  name: {
+    required: true,
+    type: String,
+  },
+  phoneNumber: {
+    required: true,
+    type: String,
+  },
+  status: {
+    required: false,
+    type: String, // waiting, current, complete
+  },
+  tokenId: {
+    required: false,
+    type: String,
+  },
 });
 
-module.exports = mongoose.model('data', dataSchema);
+module.exports = mongoose.model('token', dataSchema);
